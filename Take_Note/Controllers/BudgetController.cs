7dcde3276b6f3fdb97c9ac929f24de370121cb94
Faith Goins <faithtoriangoins@gmail.com>
2023-03-17 14:02:07 -0500

    changed controller name

---
+++ Take_Note/Controllers/BudgetController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Take_Note.Controllers
+{
+    public class BudgetController : Controller
+    {
+        private readonly IBudgetRepository repo;
+
+        public BudgetController(IBudgetRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        //Get Controller
+        public IActionResult Index()
+        {
+            var Budget = repo.GetAllBudgets();
+            return View(Budget);
+        }
+    }
+}
