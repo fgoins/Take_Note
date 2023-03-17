cbc945024f825e9a8266c8403e7faa4051e41316
Faith Goins <faithtoriangoins@gmail.com>
2023-03-17 13:00:49 -0500

    created Budget model and added Prop

---
+++ Take_Note/Models/Budget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Take_Note.Models
+{
+    public class Budget
+    {
+        public Budget() 
+        {
+        }
+
+        public int Id { get; set; }
+        public char Bill { get; set; }
+        public decimal Amount { get; set; }
+        public  char DueDate { get; set; }
+
+    }
+}
