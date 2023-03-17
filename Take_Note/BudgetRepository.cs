194de383b27fd5b6b9a82e2f26eced1f7fcace95
Faith Goins <faithtoriangoins@gmail.com>
2023-03-17 13:20:22 -0500

    add budget class and IDbconnection

---
+++ Take_Note/BudgetRepository.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using System;
+using System.Data;
+using Take_Note.Models;
+
+namespace Take_Note
+{
+    public class BudgetRepository : IBudgetRepository
+    {
+        private readonly IDbConnection _conn;
+
+        public BudgetRepository(IDbConnection conn) 
+        {
+          _conn = conn;
+        }
+        public IEnumerable<Budget> GetAllBudgets()
+        {
+            return _conn.Query<Budget>("Select * From Budget");
+        }
+    }
+}
