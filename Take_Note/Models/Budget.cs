44030a9cd265c7aaa7f2da5964313ed92f38297a
Faith Goins <faithtoriangoins@gmail.com>
2023-03-21 15:33:57 -0500

    added dates list to drop down

--- Take_Note/Models/Budget.cs
+++ Take_Note/Models/Budget.cs
@@ -12,8 +12,9 @@ namespace Take_Note.Models
         public decimal Amount { get; set; }
         public  string DueDate { get; set; }
         public IEnumerable<BillType> billTypes { get; set; }
-        public List<string> Dates { get; set; } = new List<string> {"1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th" +
-            "10th", "11th", "12th"};
+        public List<string> Dates { get; set; } = new List<string> {"1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th",
+            "10th", "11th", "12th","13th", "14th","15th","16th","17th", "18th","19th","20th","21st","22nd","23rd","24th","25th","26th",
+            "27th","28th","29th", "30th", "31st"};
 
     }
 }
